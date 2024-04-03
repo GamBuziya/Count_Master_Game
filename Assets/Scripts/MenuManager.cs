41b5dffe1fe2e195c887f8e3b84966ecb3262d24
GamBuziya <118631209+GamBuziya@users.noreply.github.com>
2024-04-03 23:52:11 +0300

    Animated UI

--- Assets/Scripts/MenuManager.cs
+++ Assets/Scripts/MenuManager.cs
@@ -1,13 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
+using DG.Tweening;
 using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
 
 public class MenuManager : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [Header("Active UI")]
+    [SerializeField] private TextMeshProUGUI _tapToPlayText;
+    [SerializeField] private Ease _motionType;
+    [SerializeField] private Image _hand;
     void Start()
     {
-        
+        _tapToPlayText.transform.DOScale(1.2f, 0.5f).SetLoops(1000, LoopType.Yoyo).SetEase(_motionType);
+        _hand.transform.DOMoveX(260, 1f).SetLoops(1000, LoopType.Yoyo).SetEase(_motionType);
     }
 
     // Update is called once per frame
@@ -15,4 +22,5 @@ public class MenuManager : MonoBehaviour
     {
         
     }
+    
 }
